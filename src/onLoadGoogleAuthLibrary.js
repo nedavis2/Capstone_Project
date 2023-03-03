002943128e2126cdda2fb1356cc87412eed11591
Logangalo <lww1117@gmail.com>
2023-03-02 21:12:31 -0500

    Added some possibles

---
+++ src/onLoadGoogleAuthLibrary.js
@@ -0,0 +1,13 @@
+function onLoadGoogleAuthLibrary() {
+  console.log('Google 3P Authorization library loaded');
+  
+  // Initialize the Google Sign-In button
+  gapi.load('auth2', function() {
+    gapi.auth2.init({
+      client_id: '<your-client-id>',
+      scope: 'email profile openid'
+    }).then(function() {
+      console.log('Google Sign-In initialized');
+    });
+  });
+}
