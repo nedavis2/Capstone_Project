d7920ee5469e7de44f12947afe54b6bf06522256
Logangalo <77752778+lww1117@users.noreply.github.com>
2023-03-04 20:28:24 -0500

    added code location

--- src/python/DBconnect.py
+++ src/python/DBconnect.py
@@ -11,6 +11,7 @@ try:
     cnx = mysql.connector.connect(user='root', password='', host='localhost', database='capstone_project')
     cursor = cnx.cursor()
 
+    #Your code goes between here
     query = ("SELECT player, team, SUM(rec) AS sumRec FROM nfl_pass_rush_receive_raw_data WHERE player = 'a.j. brown'")
 
     cursor.execute(query, ())
@@ -19,7 +20,7 @@ try:
         print("{}, {}, {}".format(
             player, team, sumRec))
 
-
+    #and here
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your user name or password")
