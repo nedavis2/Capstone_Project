4868bdc2e84a2565a5861753143442fe0e14c4b4
nedavis2 <77697088+nedavis2@users.noreply.github.com>
2023-03-06 14:42:35 -0500

    name changes and more csv files

--- Data/table_splitter.py
+++ Data/table_splitter.py
@@ -8,7 +8,7 @@ data = pd.read_csv('Data\\nfl_data.csv')
 # split main dataframe into useful parts
 game_data = data[['game_id', 'vis_team', 'home_team', 'vis_score', 'home_score', 'OT', 'game_date']].copy()
 weather_data = data[['game_id', 'Temperature', 'Humidity', 'Wind_Speed', 'Roof', 'Surface']].copy()
-player_data = data[['game_id', 'player_id', 'player', 'pos', 'team']].copy()
+player_description = data[['game_id', 'player_id', 'player', 'pos', 'team']].copy()
 
 # drop unneeded columns for team stats
 team_data = data.drop(columns=['Temperature', 'Humidity', 'Wind_Speed', 'Roof', 'Surface'])
@@ -31,6 +31,7 @@ player_stats = player_data4.groupby(['game_date', 'game_id', 'player_id', 'playe
 # remove unnecessary dataframe rows
 game_data_unique = game_data.drop_duplicates('game_id')
 weather_data_unique = weather_data.drop_duplicates('game_id')
+player_description_unique = player_description.drop_duplicates(['player'])
 
 # remove old data files so new ones can be created
 if (os.path.exists('Data\\game_data.csv')) :
@@ -39,13 +40,22 @@ if (os.path.exists('Data\\game_data.csv')) :
 if (os.path.exists('Data\\weather_data.csv')) :
     os.remove('Data\\weather_data.csv')
 
+if (os.path.exists('Data\\player_data.csv')) :
+    os.remove('Data\\player_data.csv')
+
 if (os.path.exists('Data\\team_stats.csv')) :
     os.remove('Data\\team_stats.csv')
+
+if (os.path.exists('Data\\player_stats.csv')) :
+    os.remove('Data\\player_stats.csv')
 
 # take dataframes and export to csv files
 game_data_unique.to_csv('Data\\game_data.csv', index=False)
 weather_data_unique.to_csv('Data\\weather_data.csv', index=False)
+player_description_unique.to_csv('Data\\player_data.csv', index=False)
 team_stats.to_csv('Data\\team_stats.csv', index=False)
+player_stats.to_csv('Data\\player_stats.csv', index=False)
 
 # testing print statements
-print(player_stats.loc[player_stats['pos'] == 'QB'])
+#print(player_stats.loc[player_stats['pos'] == 'WR'])
+print(player_description_unique)
