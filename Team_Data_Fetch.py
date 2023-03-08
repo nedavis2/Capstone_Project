b42bc5f12c6ccbe7469e880d58ecd27afc599747
Kevin-Hayes-UNCG <89085983+Kevin-Hayes-UNCG@users.noreply.github.com>
2023-03-08 11:31:07 -0500

    Separated player and team stats

--- Team_Data_Fetch.py
+++ Team_Data_Fetch.py
@@ -14,33 +14,61 @@ db_password = ""
 db_user = "root"
 
 
-def wide_receiver_targets(team : str):
+def player_wide_receiver_targets(player_id : str):
   pass
-def wide_receiver_rec(team : str):
+def player_wide_receiver_rec(player_id : str):
   pass
-def wide_receiver_rec_td(team : str):
+def player_wide_receiver_rec_td(player_id : str):
   pass
-def wide_receiver_rec_yds(team : str):
-  pass
-
-def tight_end_targets(team : str):
-  pass
-def tight_end_rec(team : str):
-  pass
-def tight_end_rec_td(team : str):
-  pass
-def tight_end_rec_yds(team : str):
+def player_wide_receiver_rec_yds(player_id : str):
   pass
 
-def running_back_rush_td(team : str):
+def player_tight_end_targets(player_id : str):
   pass
-def running_back_rush_att(team : str):
+def player_tight_end_rec(player_id : str):
   pass
-def running_back_rush_yds(team : str):
+def player_tight_end_rec_td(player_id : str):
   pass
-def running_back_rec_td(team : str):
-  pass
-def running_back_rec(team : str):
+def player_tight_end_rec_yds(player_id : str):
   pass
 
+def player_running_back_rush_td(player_id : str):
+  pass
+def player_running_back_rush_att(player_id : str):
+  pass
+def player_running_back_rush_yds(player_id : str):
+  pass
+def player_running_back_rec_td(player_id : str):
+  pass
+def player_running_back_rec(player_id : str):
+  pass
 
+#_________________________________________________
+def team_wide_receiver_targets(team : str):
+  pass
+def team_wide_receiver_rec(team : str):
+  pass
+def team_wide_receiver_rec_td(team : str):
+  pass
+def team_wide_receiver_rec_yds(team : str):
+  pass
+
+def team_tight_end_targets(team : str):
+  pass
+def team_tight_end_rec(team : str):
+  pass
+def team_tight_end_rec_td(team : str):
+  pass
+def team_tight_end_rec_yds(team : str):
+  pass
+
+def team_running_back_rush_td(team : str):
+  pass
+def team_running_back_rush_att(team : str):
+  pass
+def team_running_back_rush_yds(team : str):
+  pass
+def team_running_back_rec_td(team : str):
+  pass
+def team_running_back_rec(team : str):
+  pass
