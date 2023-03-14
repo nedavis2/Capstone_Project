bace0a29c8dd0c55d3f628d2a066c56b9c04713c
Logangalo <lww1117@gmail.com>
2023-03-14 16:54:23 -0400

    testPull nothingburger

--- Database/testPull.php
+++ Database/testPull.php
@@ -3,7 +3,6 @@
 
 $nfl_prr_data = file_get_contents('https://stewart-gibson.shinyapps.io/NFL_All_Data_Downloader/_w_1aab92e6/session/1970078c2acb24eaf4c4bbefb546b354/download/downloadData?w=1aab92e6');
 $data = str_getcsv($nfl_prr_data);
-//echo $data;
 
 
 
