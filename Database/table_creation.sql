3329cba51e1cf6ee86ed5ad9a779b6c69b91d518
nedavis2 <77697088+nedavis2@users.noreply.github.com>
2023-03-14 15:53:11 -0400

    minor data changes

--- Database/table_creation.sql
+++ Database/table_creation.sql
@@ -86,7 +86,7 @@ CREATE TABLE game_stats_team (
     rec_drops                       INT(4),
     offense                         INT(4),
     off_pct                         INT(4),
-    foreign key (player_id) references player(player_id)
+    foreign key (game_id) references game(game_id)
 );
 CREATE TABLE game_stats_player (
     game_date                       DATE            not null,
