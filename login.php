0ad80ed0e3f407e8cafdb23a9216a4f3d2339d5b
Clayton Winters <crwinter@uncg.edu>
2023-03-15 23:56:15 -0400

    Adding Google Login button

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$clientID = '418675902686-6p06o7996m77v44jng2plhd969plknl0.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = 'http://localhost/Capstone_Project/src/index.php';
+
+//creating client request to google
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+
+// authenticate ID token from Google Sign-In
+if (isset($_GET['id_token'])) {
+  $id_token = $_GET['id_token'];
+  $payload = $client->verifyIdToken($id_token);
+  
+  if ($payload) {
+    $userid = $payload['sub'];
+    // get profile info 
+    $email = $payload['email'];
+    $name = $payload['name'];
+
+    // use this profile info to create an account in your website and make the user logged in.
+  } else {
+    // Invalid ID token
+    echo "Invalid ID token.";
+  }
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+}
+?>
---
+++ sign_in_button.js
@@ -0,0 +1,27 @@
+var googleUser = {};
+var startApp = function () {
+    gapi.load('auth2', function () {
+        auth2 = gapi.auth2.init({
+            client_id: '418675902686-6p06o7996m77v44jng2plhd969plknl0.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+        });
+        attachSignin(document.getElementById('customBtn'));
+    });
+};
+
+function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function (googleUser) {
+            // Get the user's ID token
+            var id_token = googleUser.getAuthResponse().id_token;
+
+            // Redirect the user to the PHP script with the ID token as a URL parameter
+            window.location.href = 'login.php?id_token=' + id_token;
+        }, function (error) {
+            alert(JSON.stringify(error, undefined, 2));
+        });
+}
+
+// Call startApp when the page is fully loaded
+document.addEventListener('DOMContentLoaded', startApp);
