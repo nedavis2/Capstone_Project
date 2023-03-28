01d7fbe02017d5d72aad743b2840112756e3ffe3
nedavis2 <77697088+nedavis2@users.noreply.github.com>
2023-03-28 16:14:46 -0400

    stat insert start

--- Database/insert.php
+++ Database/insert.php
@@ -40,8 +40,22 @@ try{
                     VALUES (:game_id, :teamAway, :teamHome, :scoreAway, :scoreHome, :game_date, :OT_yn)";
     $query_str_weather = "INSERT INTO weather
                     VALUES (:game_id, :temperature, :humidity, :wind_speed, :roof, :surface)";
-    //$query_str_tStats = "INSERT INTO game_stats_team VALUES";
-    //$query_str_pStats = "INSERT INTO game_stats_player VALUES";
+    $query_str_tStats = "INSERT INTO game_stats_team 
+                    VALUES (:game_date, :game_id, :team, :pass_comp, :pass_att, :pass_yds, :pass_td, :pass_int, :pass_sacked,
+                            :pass_sacked_yds, :pass_long, :pass_rating, :rush_att, :rush_yds, :rush_td, :rush_long, :targets,
+                            :rec, :rec_yds, :rec_td, :rec_long, :fumbles_lost, :rush_scrambles, :designed_rush_att, :comb_pass_rush_play,
+                            :comb_pass_play, :comb_rush_play, :two_point_conv, :total_ret_td, :offensive_fumble_recovery_td, :pass_yds_bonus,
+                            :rush_yds_bonus, :rec_yds_bonus, :pass_target_yds, :pass_poor_throws, :pass_blitzed, :pass_hurried,
+                            :rush_yds_before_contact, :rush_yac, :rush_broken_tackles, :rec_air_yds, :rec_yac, :rec_drops, :offense,
+                            :off_pct, :yds_per_rec, :yds_per_rush, :yds_per_target)";
+    $query_str_pStats = "INSERT INTO game_stats_player 
+                    VALUES (:game_date, :game_id, :player_id, :player, :pos, :pass_comp, :pass_att, :pass_yds, :pass_td, :pass_int, :pass_sacked,
+                            :pass_sacked_yds, :pass_long, :pass_rating, :rush_att, :rush_yds, :rush_td, :rush_long, :targets,
+                            :rec, :rec_yds, :rec_td, :rec_long, :fumbles_lost, :rush_scrambles, :designed_rush_att, :comb_pass_rush_play,
+                            :comb_pass_play, :comb_rush_play, :two_point_conv, :total_ret_td, :offensive_fumble_recovery_td, :pass_yds_bonus,
+                            :rush_yds_bonus, :rec_yds_bonus, :pass_target_yds, :pass_poor_throws, :pass_blitzed, :pass_hurried,
+                            :rush_yds_before_contact, :rush_yac, :rush_broken_tackles, :rec_air_yds, :rec_yac, :rec_drops, :offense,
+                            :off_pct, :yds_per_rec, :yds_per_rush, :yds_per_target)";
 
 
     // insert into player table
