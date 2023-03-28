6afe19dc84b0b5a4c97d4ee8920b4e232fffa0b3
Kevin-Hayes-UNCG <89085983+Kevin-Hayes-UNCG@users.noreply.github.com>
2023-03-28 16:13:00 -0400

    Made individual functions

--- machine learning/function_generation.py
+++ machine learning/function_generation.py
@@ -19,7 +19,7 @@ def gen_functions(stat):
     out_team = '''def team_quarterback_%s(team : str) -> tuple((list[int], list[date])):
     retreived_data = "%s"
     position = "QB"
-    return _retrieve_team_data(team,retreived_data = retreived_data, table_name = used_table_name, weekly = True, position = position)'''
+    return _retrieve_team_data(team,retreived_data = retreived_data, table_name = used_table_name, weekly = True, position = position)'''%(stat, stat)
 
 
     print(out_total)
@@ -28,5 +28,21 @@ def gen_functions(stat):
 
     print(out_team)
 
+def gen_out_total(stat):
+    print('''def player_quarterback_%s_total(player_id : str) -> int:
+    retreived_data = "%s"
+    position = "QB"
+    return  _retrieve_player_total_data(player_id, retreived_data, table_name = used_table_name, position = position)'''%(stat, stat))
 
+def gen_out_week(stat):
+    print('''def player_quarterback_%s_weekly(player_id : str) -> tuple((list[int], list[date])):
+  retreived_data = "%s"
+  position = "QB"
+  return _retrieve_player_time_data(player_id, retreived_data, table_name = used_table_name, weekly = True, position = position)
+  '''%(stat,stat))
+def gen_out_team(stat):
+    print('''def team_quarterback_%s(team : str) -> tuple((list[int], list[date])):
+    retreived_data = "%s"
+    position = "QB"
+    return _retrieve_team_data(team,retreived_data = retreived_data, table_name = used_table_name, weekly = True, position = position)'''%(stat, stat))
 gen_functions("pass_att")
