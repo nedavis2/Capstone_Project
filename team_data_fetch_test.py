cb759612a8a59f4b05f268045f7e6dc5ca956882
Logangalo <lww1117@gmail.com>
2023-03-29 13:48:34 -0400

    Fiexed team_data_fetch_test

--- team_data_fetch_test.py
+++ team_data_fetch_test.py
@@ -5,68 +5,11 @@ from team_data_fetch import *
 #TODO
 
 
-#Player WR stuff
-def player_wr_fetch(player_id: str):
-    print('player_wide_receiver_targets_weekly')
-    print(player_wide_receiver_targets_weekly(player_id))
-    print('player_wide_receiver_targets_total')
-    print(player_wide_receiver_targets_total(player_id))
-    print('player_wide_receiver_rec_total')
-    print(player_wide_receiver_rec_total(player_id))
-    print('player_wide_receiver_rec_td_total')
-    print(player_wide_receiver_rec_td_total(player_id))
-    print('player_wide_receiver_rec_yds_total')
-    print(player_wide_receiver_rec_yds_total(player_id))
-    print('player_wide_receiver_rec_weekly')
-    print(player_wide_receiver_rec_weekly(player_id))
-    print('player_wide_receiver_rec_td_weekly')
-    print(player_wide_receiver_rec_td_weekly(player_id))
-    print('player_wide_receiver_rec_yds_weekly')
-    print(player_wide_receiver_rec_yds_weekly(player_id))
-    print('player_wide_receiver_targets_monthly')
-    print(player_wide_receiver_targets_monthly(player_id))
-    print('player_wide_receiver_rec_monthly')
-    print(player_wide_receiver_rec_monthly(player_id))
-    print('player_wide_receiver_rec_td_monthly')
-    print(player_wide_receiver_rec_td_monthly(player_id))
-    print('player_wide_receiver_rec_yds_monthly')
-    print(player_wide_receiver_rec_yds_monthly(player_id))
     
     
 #player rb stuff
 
-# #Player RB stuff
-def player_rb_fetch(player_id: str):
-    print('player_running_back_rush_td_total')
-    print(player_running_back_rush_td_total(player_id))
-    print('player_running_back_rush_att_total')
-    print(player_running_back_rush_att_total(player_id))
-    print('player_running_back_rush_yds_total')
-    print(player_running_back_rush_yds_total(player_id))
-    print('player_running_back_rec_td_total')
-    print(player_running_back_rec_td_total(player_id))
-    print('player_running_back_rec_total')
-    print(player_running_back_rec_total(player_id))
-    print('player_running_back_rush_td_weekly')
-    print(player_running_back_rush_td_weekly(player_id))
-    print('player_running_back_rush_att_weekly')
-    print(player_running_back_rush_att_weekly(player_id))
-    print('player_running_back_rush_yds_weekly')
-    print(player_running_back_rush_yds_weekly(player_id))
-    print('player_running_back_rec_td_weekly')
-    print(player_running_back_rec_td_weekly(player_id))
-    print('player_running_back_rec_weekly')
-    print(player_running_back_rec_weekly(player_id))
-    print('player_running_back_rush_td_monthly')
-    print(player_running_back_rush_td_monthly(player_id))
-    print('player_running_back_rush_att_monthly')
-    print(player_running_back_rush_att_monthly(player_id))
-    print('player_running_back_rush_yds_monthly')
-    print(player_running_back_rush_yds_monthly(player_id))
-    print('player_running_back_rec_td_monthly')
-    print(player_running_back_rec_td_monthly(player_id))
-    print('player_running_back_rec_monthly')
-    print(player_running_back_rec_monthly(player_id))
+
     
 #'AdamDa01'
 #'TonyRo00'
@@ -74,59 +17,55 @@ def player_rb_fetch(player_id: str):
 
 
 def player_receiver_view_1(player_id: str):
-    print('player_wide_receiver_targets_weekly #')
-    print(player_wide_receiver_targets_weekly(player_id))
-    print('player_wide_receiver_rec_weekly')
-    print(player_wide_receiver_rec_weekly(player_id))
-    print('player_wide_receiver_rec_td_weekly')
-    print(player_wide_receiver_rec_td_weekly(player_id))
-    print('player_wide_receiver_rec_yds_weekly')
-    print(player_wide_receiver_rec_yds_weekly(player_id))
+    return ('player_wide_receiver_targets_weekly #' + str(player_wide_receiver_targets_weekly(player_id))
+    + 'player_wide_receiver_rec_weekly' + str(player_wide_receiver_rec_weekly(player_id))
+    + 'player_wide_receiver_rec_td_weekly' + str(player_wide_receiver_rec_td_weekly(player_id))
+    + 'player_wide_receiver_rec_yds_weekly' + str(player_wide_receiver_rec_yds_weekly(player_id)))
     
 def player_receiver_view_1_month(player_id: str):
-    print('player_wide_receiver_targets_monthly #')
-    print(player_wide_receiver_targets_monthly(player_id))
-    print('player_wide_receiver_rec_monthly')
-    print(player_wide_receiver_rec_monthly(player_id))
-    print('player_wide_receiver_rec_td_monthly')
-    print(player_wide_receiver_rec_td_monthly(player_id))
-    print('player_wide_receiver_rec_yds_monthly')
-    print(player_wide_receiver_rec_yds_monthly(player_id))
+    return(('player_wide_receiver_targets_monthly #')
+    +str(player_wide_receiver_targets_monthly(player_id))
+    +str('player_wide_receiver_rec_monthly')
+    +str(player_wide_receiver_rec_monthly(player_id))
+    +str('player_wide_receiver_rec_td_monthly')
+    +str(player_wide_receiver_rec_td_monthly(player_id))
+    +str('player_wide_receiver_rec_yds_monthly')
+    +str(player_wide_receiver_rec_yds_monthly(player_id)))
    
 def player_receiver_view_1_total(player_id: str):
-    print('player_wide_receiver_targets_total #')
-    print(player_wide_receiver_targets_total(player_id))
-    print('player_wide_receiver_rec_total')
-    print(player_wide_receiver_rec_total(player_id))
-    print('player_wide_receiver_rec_td_total')
-    print(player_wide_receiver_rec_td_total(player_id))
-    print('player_wide_receiver_rec_yds_total')
-    print(player_wide_receiver_rec_yds_total(player_id))
+    return(('player_wide_receiver_targets_total #')
+    +str(player_wide_receiver_targets_total(player_id))
+    +str('player_wide_receiver_rec_total')
+    +str(player_wide_receiver_rec_total(player_id))
+    +str('player_wide_receiver_rec_td_total')
+    +str(player_wide_receiver_rec_td_total(player_id))
+    +str('player_wide_receiver_rec_yds_total')
+    +str(player_wide_receiver_rec_yds_total(player_id)))
     
 def player_rusher_view_1(player_id: str):
-    print('player_running_back_rush_td_weekly')
-    print(player_running_back_rush_td_weekly(player_id))
-    print('player_running_back_rush_att_weekly')
-    print(player_running_back_rush_att_weekly(player_id))
-    print('player_running_back_rush_yds_weekly')
-    print(player_running_back_rush_yds_weekly(player_id))
+    return(('player_running_back_rush_td_weekly')
+    +str(player_running_back_rush_td_weekly(player_id))
+    +str('player_running_back_rush_att_weekly')
+    +str(player_running_back_rush_att_weekly(player_id))
+    +str('player_running_back_rush_yds_weekly')
+    +str(player_running_back_rush_yds_weekly(player_id)))
 
     
 def player_rusher_view_1_month(player_id: str): 
-    print('player_running_back_rush_td_monthly')
-    print(player_running_back_rush_td_monthly(player_id))
-    print('player_running_back_rush_att_monthly')
-    print(player_running_back_rush_att_monthly(player_id))
-    print('player_running_back_rush_yds_monthly')
-    print(player_running_back_rush_yds_monthly(player_id))
+    return(('player_running_back_rush_td_monthly')
+    +str(player_running_back_rush_td_monthly(player_id))
+    +str('player_running_back_rush_att_monthly')
+    +str(player_running_back_rush_att_monthly(player_id))
+    +str('player_running_back_rush_yds_monthly')
+    +str(player_running_back_rush_yds_monthly(player_id)))
     
 def player_rusher_view_1_total(player_id: str):   
-    print('player_running_back_rush_td_total')
-    print(player_running_back_rush_td_total(player_id))
-    print('player_running_back_rush_att_total')
-    print(player_running_back_rush_att_total(player_id))
-    print('player_running_back_rush_yds_total')
-    print(player_running_back_rush_yds_total(player_id))
+    return(('player_running_back_rush_td_total')
+    +str(player_running_back_rush_td_total(player_id))
+    +str('player_running_back_rush_att_total')
+    +str(player_running_back_rush_att_total(player_id))
+    +str('player_running_back_rush_yds_total')
+    +str(player_running_back_rush_yds_total(player_id)))
 
 
 
@@ -134,6 +73,7 @@ def player_rusher_view_1_total(player_id: str):
 
 
 def player_primary_view(player_id: str, pos: str):
+    view = None
     match pos:
         case 'RB':
             view = player_rusher_view_1( player_id) + "#" + player_rusher_view_1_month( player_id) + "#" 
@@ -142,9 +82,10 @@ def player_primary_view(player_id: str, pos: str):
         case 'WR':
             view = str(player_receiver_view_1(player_id) + "#" + player_receiver_view_1_month(player_id) + "#"
             + player_receiver_view_1_total(player_id))
+    return view
             
             
-player_primary_view('AdamDa01','WR')
+print(player_primary_view('AdamDa01','WR'))
 
 
 
