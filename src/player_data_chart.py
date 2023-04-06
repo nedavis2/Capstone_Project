4c96a847c62d12d514c4833d7425a8ea3a41d71c
Logangalo <lww1117@gmail.com>
2023-04-06 15:07:54 -0400

    add bootstrap 5, may/may not keep

--- src/player_data_chart.py
+++ src/player_data_chart.py
@@ -3,11 +3,30 @@ import mysql.connector
 from mysql.connector import errorcode
 from team_data_fetch_test import *
 
-if __name__ == "__main__":
-       def some_function():
+
+try:
+
+    cnx = mysql.connector.connect(
+        user='root', password='', host='localhost', database='capstone_project')
+    cursor = cnx.cursor()
+    if __name__ == "__main__":
+        def some_function():
 
             player_id, pos = sys.argv[1].split(',')
-            
+
             print(player_primary_view(player_id, pos))
 
-some_function()
+    some_function()
+
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+
+    cnx.close()
+    cursor.close()
